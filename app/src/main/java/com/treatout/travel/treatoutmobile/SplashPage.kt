2a4312f3404386a9f6467b273b57c583e042b3d9
Christian Hizon <christiangerardhizon@gmail.com>
2018-09-08 10:25:26 +0800

    splash page

--- app/src/main/java/com/treatout/travel/treatoutmobile/SplashPage.kt
+++ app/src/main/java/com/treatout/travel/treatoutmobile/SplashPage.kt
@@ -1,12 +1,45 @@
 package com.treatout.travel.treatoutmobile
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import java.util.*
 
 class SplashPage : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_page)
+
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
     }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
 }
